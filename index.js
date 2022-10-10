01538912d73ca496429241124b707c7b3c7f702d
Brandon Jenkins <90408885+imbrandonj@users.noreply.github.com>
2022-10-09 19:50:11 -0500

    big update
    terms.js, cyber terms, design formatting, "idk" value

--- index.js
+++ index.js
@@ -1,46 +1,14 @@
+
 // global constants and variables
 const DISPLAY_TERM_DEF = document.getElementById("definition");  // html paragraph displaying term definition
 const TEXTBOX = document.getElementById("textBox");  // text input type
 let term = "empty string";  // the term is global to be passed between functions and randomly selected on load
 
-// TO DO: implement PROBLEMSET as a json file
-const PROBLEMSET = {
-    "internal threat": "An employee stealing company data could be an example of which kind of threat actor?",
-    "hacker": "A general term used to describe any individual who uses their technical knowledge to gain unauthorized access to an organization",
-    "script kiddie": "an individual who carries out an attack by using scripts or programs written by more advanced hackers",
-    "opportunistic attack": "A hacker scans hundreds of IP addresses randomly on the internet until they find an exploitable target. What kind of attack is this?",
-    "staging": "Preparing a computer to perform additional tasks in the attack",
-    "reconnaissance": "the process of gathering information about an organization, including system hardware information, network configuration, and individual user information",
-    "layering": "the process of implementing multiple security measures to protect the same asset",
-    "reconnaissance": "which phase of an attack does the attacker gather information about the target?",
-    "confidentiality": "ensures that data is not disclosed to unintended persons",
-    "cyber terrorists": "generally use the internet to carry out activities such as disrupting network-dependent institutions",
-    "non-repudiation": "The ability to prove that a sender undeniably sent a message",
-    "privacy": "the protection and confidentiality of personal information",
-    "integrity": "protection against alteration",
-    "application layer": "this layer of the security model includes user management and web application security",
-    "host layer": "this layer of the security model includes log management",
-    "physical layer": "this layer of the security model includes environmental controls",
-    "data layer": "this layer of the security model includes cryptography",
-    "perimeter layer": "this layer of the security model includes securing firewalls",
-    "employees": "the single greatest threat to network security",
-    "network layer": "this layer of the security model includes penetration testing",
-    "botnet": "A collection of zombie computers have been set up to collect personal information. Which type of malware do the zombie computers represent?",
-    "fileless virus": "this kind of virus operates only in memory and usually exploits a trusted application like PowerShell to circumvent traditional endpoint security solutions",
-    "worm": "a self-replicating program",
-    "ransomware": "A type of malware that prevents the system from being used until the victim pays the attacker money",
-    "remote access trojan": "this kind of malware provides an attacker with administrative control over a target computer through a backdoor",
-    "spyware": "monitors the actions you take on your machine and sends the information back to its originating source",
-    "Trojan horse": "a malicious program disguised as legitimate software",
-    "virus": "a program that attempts to damage a computer system and replicate itself to other computer systems",
-    "social engineering": "Any attack involving human interaction of some kind",
-    "social engineering": "Keylogging, impersonation, and shoulder surfing are all examples of this type of attack",
-    "superuser account": "The root account has all privileges and no barriers. Also known as...",
-    "data breach": "when confidential or protected data is exposed",
-    
-};
+// Problem set in terms.js file
+const PROBLEMSET = CYBERSECURITY;
 
-const ALL_TERM_DEF = Object.values(PROBLEMSET)  // all term definitions
+// Get term definitions
+const ALL_TERM_DEF = Object.values(PROBLEMSET) 
 
 
 // functions and events:
@@ -85,7 +53,11 @@ function validateAnswer(input, answer) {
         TEXTBOX.value = "";
         DISPLAY_TERM_DEF.removeChild(DISPLAY_TERM_DEF.firstChild);
         loadTerm();
-    } else {
+    } 
+    else if ( input == "idk" ) {
+        TEXTBOX.value = term;
+    } 
+    else {
         TEXTBOX.value = "";   
     }
 };
@@ -94,7 +66,7 @@ function validateAnswer(input, answer) {
 // give answer button event
 function giveAnswer() {
     TEXTBOX.value = term;  // propagates the answer to the text input box
-}
+};
 
 
 // on page load a random term is selected
