6c5355343ec004c43ddc72c9c359f5849e94290a
Brandon <imbrandonj42@gmail.com>
2024-03-19 14:53:59 -0500

    adds terms to `pmTerms.js`

--- src/terms/pmTerms.js
+++ src/terms/pmTerms.js
@@ -30,6 +30,43 @@ const PMTERMS = {
     'Execution',
   'The project team is disbanded after approval in this Project Life Cycle phase':
     'Closing',
+  "A brief document that justifies the investments made for a project and describes how a particular investment is in accordance with the organization's policy.":
+    'Business Case',
+  'The act of generating different plans for achieving project goals.':
+    'Alternatives Identification',
+  '(True/False): In the Options section of a business case, always include a "Do Nothing" option.':
+    'True',
+  'A generic name for protocols that carry voice traffic over data networks.':
+    'Voice Over Internet Protocol (VOIP)',
+  'It shows the sequence of events and the flow of inputs and outputs between elements in a process or system.':
+    'Process Flowchart',
+  'A metric to calculate whether an asset is worth the cost of deploying and maintaining it.':
+    'ROI',
+  'Compares the financial benefit of the project to the cost, and it is written as a percentage.':
+    'ROI',
+  'A mutually binding agreement that details the obligations of the buyer and vendor.':
+    'Contract',
+  'The three main values of ROI': 'Benefit, Cost, Net Profit',
+  'The difference between the benefit and the cost.': 'Net Profit',
+  'The money generated or explicitly saved due to the profit; financial value or revenue':
+    'Benefit',
+  'Compliance-mandatory characteristics for specific products, services, or processes.':
+    'regulations',
+  'The statements that must be taken to be true in order to begin project planning.':
+    'assumptions',
+  'Any unexpected event that can affect your project. The effect might be either positive or negative, and the impact might be on any aspect of the project.':
+    'risks',
+  'Threats to the financial benefits of the project.': 'risks',
+  'A project selection decision model that analyzes the predicted value of the completed projects in different ways.':
+    'Benefit Measurement Models',
+  'Organizational performance measures unrelated to financial performance that assess how an organization contributes to society.':
+    'ESG',
+  'A control point event in a project with zero duration that triggers a reporting requirement or requires sponsor or customer approval before proceeding with the project.':
+    'milestones',
+  'Individuals and organizations who are external to the company and provide specialized support to tasks such as installation, customization, training, or support.':
+    'business partners',
+  "Includes the company's vision, mission statement, values, and brand.":
+    'corporate identity',
 };
 
 export default PMTERMS;
