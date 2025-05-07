810e750f1fe63229efa940dab95a1f6168fdd807
Brandon Jenkins <imbrandonj42@gmail.com>
2025-05-07 11:28:30 -0500

    CCNA topic 1 added

--- src/terms/netTerms.js
+++ src/terms/netTerms.js
@@ -1,6 +1,31 @@
 const NETTERMS = {
   'A digital telecommunications network which allows nodes to share resources.':
     'computer network',
+  'These firewalls are software applications that filter traffic entering and exiting a host machine, like a PC.':
+    'host-based',
+  'A [...] has many network interfaces for end hosts to connect to.': 'switch',
+  'A [...] is a device that accesses a service made available by a server.':
+    'client',
+  'A [...] is a device that provides functions or services for clients.':
+    'server',
+  'A [...] is used to send data over the Internet.': 'router',
+  'A [...] monitors and controls network traffic based on configured rules.':
+    'firewall',
+  'A [...] provides connectivity to hosts within the same LAN.': 'switch',
+  'A computer network is a digital telecommunications network which allows [...] to share resources.':
+    'nodes',
+  "A firewall which combines a traditional firewall with more advanced filtering functionalities is known as a '[...] firewall'.":
+    'next-generation',
+  '[True or False] a single device be both a client and a server.': 'true',
+  '[True or False] routers have fewer network interfaces than switches.':
+    'true',
+  '[True or False] routers have more network interfaces than switches.':
+    'false',
+  'What does LAN stand for?': 'Local Area Network',
+  "What kind of network device is a Cisco 'Catalyst'?": 'switch',
+  'What kind of network device is a Cisco ASA?': 'firewall',
+  'What kind of network device is a Cisco Firepower?': 'firewall',
+  'What kind of network device is a Cisco ISR?': 'router',
 };
-
+('');
 export default NETTERMS;
